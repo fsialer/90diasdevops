d097e4b66a23f4fae8662451e1a2b625fd69785b
Fernando <asialer05@hotmail.com>
2025-07-09 17:53:49 -0500

    workflow python y test actualizado

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,9 +21,15 @@ def test_suma(client):
     r = client.get('/suma/3/4')
     assert json.loads(r.data)['resultado'] == 7
 
+# def test_saludo(client):
+#     r = client.get('/saludo/Rox')
+#     assert '¡Hola Rox!' in r.get_data(as_text=True)
+
 def test_saludo(client):
     r = client.get('/saludo/Rox')
-    assert '¡Hola Rox!' in r.get_data(as_text=True)
+    data = json.loads(r.data)
+    assert data['saludo'] == '¡Hola Rox!'
+    assert data['mensaje'] == 'Bienvenido a mi aplicación'
 
 def test_multiplicar(): assert multiplicar(2, 3) == 6
 def test_es_par(): assert es_par(4)
