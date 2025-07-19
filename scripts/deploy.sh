7b6e2f2d41f7390515cdd6c48a566b79f38fe6b4
Fernando <asialer05@hotmail.com>
2025-07-18 22:17:33 -0500

    cambio en el script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,23 +2,23 @@
 ENV=$1
 echo "Desplegando $ENV"
 touch ../.env
-if [[$ENV == "staging"]]; then
+if [[ $ENV == "staging" ]] ; then
     cp ../.env.staging.example ../.env
     echo "🏗️ Build de imágenes Docker"
-    docker compose -f docker-compose.staging.yml build
-    docker compose -f docker-compose.staging.yml up -d
-elif [[$ENV == "production"]]; then
-        cp ../.env.prod.example ../.env
-        echo "🏗️ Build de imágenes Docker"
-        docker compose -f docker-compose.prod.yml build
-        docker compose -f docker-compose.prod.yml up -d
+    docker compose -f ../docker-compose.staging.yml build
+    docker compose -f ../docker-compose.staging.yml up -d
+elif [[ $ENV == "production" ]]; then
+    cp ../.env.prod.example ../.env
+    echo "🏗️ Build de imágenes Docker"
+    docker compose -f ../docker-compose.prod.yml build
+    docker compose -f ../docker-compose.prod.yml up -d
 else
-        cp ../.env.example ../.env
-        echo "🏗️ Build de imágenes Docker"
-        docker compose -f docker-compose.yml build
-        docker compose -f docker-compose.yml up -d
-        docker composer logs -f
-        docker composer down -v
+    cp ../.env.example ../.env
+    echo "🏗️ Build de imágenes Docker"
+    docker compose -f ../docker-compose.yml build
+    docker compose -f ../docker-compose.yml up -d
+    docker composer logs -f
+    docker composer down -v
 fi
 
 
