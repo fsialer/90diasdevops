12193f105b40ff40aa1737a76bf69cc5454b9f3b
Fernando <asialer05@hotmail.com>
2025-10-02 11:58:35 -0500

    dia 37 - Desplegar en multipls ambientes

---
+++ crear_runners/setup.sh
@@ -0,0 +1,33 @@
+# Añadir repo de Helm
+helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
+
+# Instalar ARC Controller
+helm install arc \
+    --namespace arc-systems \
+    --create-namespace \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+# Verificar instalación
+kubectl get pods -n arc-systems
+
+# Crear namespace para runners
+kubectl create namespace arc-runners
+
+# crear secret github
+kubectl apply -f secret_github.yml
+
+# Instalar Runner Scale Set
+
+GITHUB_CONFIG_URL="git@github.com:fsialer/90diasdevops.git"
+
+# Instalar runners
+helm install mi-runners \
+    --namespace arc-runners \
+    --set githubConfigUrl="$GITHUB_CONFIG_URL" \
+    --set githubConfigSecret=github-auth \
+    --set maxRunners=3 \
+    --set minRunners=0 \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+
+# Verificar
+kubectl get pods -n arc-runners
